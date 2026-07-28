4ef3fbc50bd13f0cf7375be489f3c7ceebf61296
Aleks Novozilovs <aleksandr.novozhilov@gmail.com>
2026-07-28 21:35:18 +0100

    Load flight data from CSV

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,4 @@
+import csv
 from flight import Flight
 
 class FlightData:
@@ -16,3 +17,17 @@ class FlightData:
             if flight.flight_number.casefold() == flight_number.casefold():
                 return flight
         return None    
+
+    def load_csv(self):
+        with open("sample_flights.csv") as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                flight = Flight(
+                    row[0],
+                    row[1],
+                    row[2],
+                    row[3],
+                    int(row[4])
+                )
+                self.add_flight(flight)
--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-from flight import Flight
 from flight_data import FlightData
 
 flight_data = FlightData()
+flight_data.load_csv()
 
-flight_1 = Flight(
-    "BA117",
-    "British Airways",
-    "London Heathrow LHR",
-    "New York JFK",
-    36000
-)
-
-flight_2 = Flight(
-    "EK2",
-    "Emirates",
-    "London Heathrow LHR",
-    "Dubai DXB",
-    38000
-)
-
-flight_data.add_flight(flight_1)
-flight_data.add_flight(flight_2)
 
 search_number = input("Enter a flight number: ")
 
